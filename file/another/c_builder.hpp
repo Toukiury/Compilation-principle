6e29e14ebf5791f7fab69214d4aeb0a85b53062e
zzzzfaker <118266736+zzzzfaker@users.noreply.github.com>
2025-05-19 23:32:24 +0800

    Update c_builder.hpp

--- file/another/c_builder.hpp
+++ file/another/c_builder.hpp
@@ -1,16 +1,18 @@
 #pragma once
 
 #include "another/builder.hpp"
+#include <string>
+#include <fstream>
 
-namespace builder {
-namespace c {
-class CBuilder : public Builder {
+namespace builder::c {
+
+class CBuilder final : public Builder {
 public:
-    void build(ir::Module &program) override;
-    void output(std::ofstream &out) override;
+    void build(ir::Module& program) noexcept override;
+    void output(std::ofstream& out) noexcept override;
+
 private:
     std::string code_;
 };
 
-} // namespace c
-} // namespace builder
+} // namespace builder::c
