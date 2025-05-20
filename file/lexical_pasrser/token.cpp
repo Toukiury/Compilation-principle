0005a29f656aa5fe7a35542fe2525fa1b744d0a7
Toukiury <135205804+Toukiury@users.noreply.github.com>
2025-05-20 11:21:31 +0800

    Update token.cpp

--- file/lexical_pasrser/token.cpp
+++ file/lexical_pasrser/token.cpp
@@ -1,181 +1,185 @@
 #include "token.hpp"
 #include <string>
-/*
-*   @brief Token类，用于表示词法分析的结果
-*/
-Token::Token(){};
-/*
-    *   @brief 装载Token
-    *   @param token_type token的类型
-    *   @param token_value token的值
-    *   @param lineno token所在行号
-    *   @param colno token所在列号
-    */
-    void Token::fill(int token_type, std::string token_value, int lineno, int colno){
-        this->token_type = token_type;
-        this->token_value = token_value;
-        this->lineno = lineno;
-        this->colno = colno;
-    };
-    /*
-    *   @brief 析构函数
-    */
-    Token::~Token(){};
-    /*
-    *   @brief 获取token的类型
-    *   @return token的类型
-    */
-    int Token:: get_token_type(){
-        return this->token_type;
-    };
-    /*
-    *   @brief 获取token所在行号
-    *   @return token所在行号
-    */
-    int Token::get_lineno(){
-        return this->lineno;
-    };
-    /*
-    *   @brief 获取token所在列号
-    *   @return token所在列号
-    */
-    int Token::get_colno(){
-        return this->colno;
-    };
-    /*
-    *   @brief 获取token的string类型值
-    *   @return token的string类型值
-    */
-    std::string Token::get_string(){
-        return this->token_value;
-    };
-    /*
-    *   @brief 获取token的int类型值
-    *   @return token的int类型值
-    */
-    long long Token::get_int(){
-        return std::stoll(this->token_value);
-    };
-    /*
-    *   @brief 获取token的double类型值
-    *   @return token的double类型值
-    */
-    double Token::get_real(){
-        return std::stod(this->token_value);
-    };
-    /*
-    *   @brief 获取token的bool类型值
-    *   @return token的bool类型值
-    */
-    bool Token::get_bool(){
-        return this->token_value == "true";
-    };
-    /*
-    *   @brief 获取token的char类型值
-    *   @return token的char类型值
-    */
-    char Token::get_char(){
-        return this->token_value[1];
-    };
 
-    /*
-    *   @brief 获取token的类型
-    *   @param token_type token的类型
-    *  @return token的类型
-    */
-    std::string Token::get_type(){
-    switch (token_type){
-        case ERROR:
-            return "ERROR";
-            break;
-        case TOKENEOF:
-            return "TOKENEOF";
-            break;
-        case ID:
-            return "ID";
-            break;
-        case KW:
-            return "KW";
-            break;
-        case INT:
-            return "INT";
-            break;
-        case REAL:
-            return "REAL";
-            break;
-        case CHAR:
-            return "CHAR";
-            break;
-        case STRING:
-            return "STRING";
-            break;
-        case BOOL:
-            return "BOOL";
-            break;
-        case OPERATOR:
-            return "OPERATOR";
-            break;
-        case SEPARATOR:
-            return "SEPARATOR";
-            break;
-        case ANNO:
-            return "ANNO";
-        default:
-            return "ERROR";
-            break;
+// Default constructor implementation
+Token::Token()
+{
+    // Empty constructor
+}
+
+/**
+ * @brief Populate token with all required data
+ * @param token_type Type identifier for the token
+ * @param token_value String representation of the token
+ * @param lineno Source line number
+ * @param colno Source column number
+ */
+void Token::fill(int token_type, std::string token_value, int lineno, int colno)
+{
+    this->token_type = token_type;
+    this->token_value = token_value;
+    this->lineno = lineno;
+    this->colno = colno;
+}
+
+// Virtual destructor implementation
+Token::~Token()
+{
+    // Empty destructor
+}
+
+/**
+ * @brief Retrieves the token type identifier
+ * @return Integer representing token type
+ */
+int Token::get_token_type()
+{
+    return token_type;
+}
+
+/**
+ * @brief Retrieves the line number in source
+ * @return Source line number
+ */
+int Token::get_lineno()
+{
+    return lineno;
+}
+
+/**
+ * @brief Retrieves the column number in source
+ * @return Source column number
+ */
+int Token::get_colno()
+{
+    return colno;
+}
+
+/**
+ * @brief Get raw token string value
+ * @return Token as string
+ */
+std::string Token::get_string()
+{
+    return token_value;
+}
+
+/**
+ * @brief Convert token value to integer
+ * @return Integer value of the token
+ */
+long long Token::get_int()
+{
+    return std::stoll(token_value);
+}
+
+/**
+ * @brief Convert token value to real number
+ * @return Real (floating point) value
+ */
+double Token::get_real()
+{
+    return std::stod(token_value);
+}
+
+/**
+ * @brief Convert token value to boolean
+ * @return Boolean value (true/false)
+ */
+bool Token::get_bool()
+{
+    return (token_value == "true");
+}
+
+/**
+ * @brief Extract character value from token
+ * @return Character value
+ */
+char Token::get_char()
+{
+    return token_value[1];
+}
+
+/**
+ * @brief Convert token type to human-readable string
+ * @return String representation of token type
+ */
+std::string Token::get_type()
+{
+    switch (token_type)
+    {
+        case ERROR:     return "ERROR";
+        case TOKENEOF:  return "TOKENEOF";
+        case ID:        return "ID";
+        case KW:        return "KW";
+        case INT:       return "INT";
+        case REAL:      return "REAL";
+        case CHAR:      return "CHAR";
+        case STRING:    return "STRING";
+        case BOOL:      return "BOOL";
+        case OPERATOR:  return "OPERATOR";
+        case SEPARATOR: return "SEPARATOR";
+        case ANNO:      return "ANNO";
+        default:        return "ERROR";
     }
-};
-    /*
-    *   @brief 字符串
-    *   @return 字符串
-    */
-    std::string Token::to_string(){
-        switch (token_type)
-        {
+}
+
+/**
+ * @brief Format token as display string with metadata
+ * @return Formatted string representation with position info
+ */
+std::string Token::to_string()
+{
+    std::string position = "[" + std::to_string(lineno) + "," + std::to_string(colno) + "]";
+    std::string type_str = get_type();
+    std::string result = "[Token] " + type_str + " ";
+    
+    switch (token_type)
+    {
         case BOOL:
-            return "[Token] " + get_type() + " " + (get_bool() ? "true" : "false") + " " + "[" + std::to_string(lineno) + "," + std::to_string(colno) + "]" ;
+            result += (get_bool() ? "true" : "false");
             break;
+            
         case CHAR:
-            return "[Token] " + get_type() + " " + get_char() + " " + "[" + std::to_string(lineno) + "," + std::to_string(colno) + "]" ;
+            result += get_char();
             break;
+            
         case STRING:
-            // 假设字符串总是由两端的引号包围
-            if (this->token_value.length() >= 2) {
-                // 使用substr去除两端的引号，开始于第1个字符并结束于倒数第2个字符
-                std::string unquoted_string = this->token_value.substr(1, this->token_value.length() - 2);
-                return "[Token] " + get_type() + " " + unquoted_string + " " + "[" + std::to_string(lineno) + "," + std::to_string(colno) + "]";
+            // Handle string formatting - remove surrounding quotes
+            if (token_value.length() >= 2) {
+                result += token_value.substr(1, token_value.length() - 2);
             } else {
-                // 如果字符串长度不足以包含两个引号，则直接返回原始值
-                return "[Token] " + get_type() + " " + this->token_value + " " + "[" + std::to_string(lineno) + "," + std::to_string(colno) + "]";
+                result += token_value;
             }
+            break;
+            
         case INT:
-            return "[Token] " + get_type() + " " + std::to_string(get_int()) + " " + "[" + std::to_string(lineno) + "," + std::to_string(colno) + "]" ;
+            result += std::to_string(get_int());
             break;
+            
         case REAL:
-            return "[Token] " + get_type() + " " + std::to_string(get_real()) + " " + "[" + std::to_string(lineno) + "," + std::to_string(colno) + "]" ;
+            result += std::to_string(get_real());
             break;
+            
         case ID:
-            return "[Token] " + get_type() + " " + get_string() + " " + "[" + std::to_string(lineno) + "," + std::to_string(colno) + "]" ;
-            break;
         case KW:
-            return "[Token] " + get_type() + " " + get_string() + " " + "[" + std::to_string(lineno) + "," + std::to_string(colno) + "]" ;
-            break;
         case OPERATOR:
-            return "[Token] " + get_type() + " " + get_string() + " " + "[" + std::to_string(lineno) + "," + std::to_string(colno) + "]" ;
-            break;
         case SEPARATOR:
-            return "[Token] " + get_type() + " " + get_string() + " " + "[" + std::to_string(lineno) + "," + std::to_string(colno) + "]" ;
-            break;
-        case TOKENEOF:
-            return "[Token] " + get_type() + " " + "[" + std::to_string(lineno) + "," + std::to_string(colno) + "]" ;
-            break;
-        case ERROR:
-            return "[Token] " + get_type() + " " + "[" + std::to_string(lineno) + "," + std::to_string(colno) + "]" ;
-            break;
         case ANNO:
-            return "[Token] " + get_type() + " " + get_string() + " " + "[" + std::to_string(lineno) + "," + std::to_string(colno) + "]" ;
-        default:
-            return "[Token] DEFAULT TOKENTYPE line : >>> "+ std::to_string(lineno) + " " + "col : >>> " +std::to_string(colno);
+            result += get_string();
             break;
-        }
-    };
+            
+        case TOKENEOF:
+        case ERROR:
+            // No additional text for these types
+            break;
+            
+        default:
+            return "[Token] DEFAULT TOKENTYPE line : >>> " + 
+                   std::to_string(lineno) + " col : >>> " + 
+                   std::to_string(colno);
+    }
+    
+    result += " " + position;
+    return result;
+}
